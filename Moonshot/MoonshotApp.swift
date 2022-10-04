953c0c5e68d1ef1a3f13319def8b89ae40e2e5b8
JooMin Choi <joomin.choi@clearscore.com>
2022-10-04 15:24:01 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by JooMin Choi on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
