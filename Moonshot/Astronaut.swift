cafc53a61f1c77341e407ca90f5e957b5aad5d00
JooMin Choi <joomin.choi@clearscore.com>
2022-10-05 14:36:41 +0100

    added json and image files + loading a specific kind of Codable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by JooMin Choi on 05/10/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
