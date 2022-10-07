40deb3257d841cdcdd4c5adfc84ef928906a675b
JooMin Choi <joomin.choi@clearscore.com>
2022-10-07 15:27:11 +0100

    incorporated list view with switch button

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,55 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
-    
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+//    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+//    let missions: [Mission] = Bundle.main.decode("missions.json")
+//
+//    let columns = [
+//        GridItem(.adaptive(minimum: 150))
+//    ]
+    @State var showingGrid = true
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
-                        }
-                    }
+            Group {
+                if showingGrid {
+                    GridLayout()
+                } else {
+                    ListLayout()
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
-            .background(.darkBackground)
-            .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if showingGrid {
+                        Button(action: {
+                            showingGrid = false
+                        }){
+                            Text("List view")
+                            Image(systemName: "list.bullet.rectangle")
+                        }
+                    } else {
+                        Button(action: {
+                            showingGrid = true
+                        }){
+                            Text("Grid view")
+                            Image(systemName: "square.grid.3x3.square")
+                        }
+                    }
             
+                }
+            }
         }
+//        ToolbarItem(placement: .navigationBarTrailing) {
+//            Button {
+//                Image(systemName: "square.grid.3x3.square")
+//            }
+            
+//            Button(action: {
+//                showingAddExpense = true
+//            }){
+//                Image(systemName: "plus")
+//            }
+//        }
+            //            ScrollView {
+            //                LazyVGrid(columns: columns) {
+            //                    ForEach(missions) { mission in
+            //                        NavigationLink {
+            //                            MissionView(mission: mission, astronauts: astronauts)
+            //                        } label: {
+            //                            VStack {
+            //                                Image(mission.image)
+            //                                    .resizable()
+            //                                    .scaledToFit()
+            //                                    .frame(width: 100, height: 100)
+            //
+            //                                VStack {
+            //                                    Text(mission.displayName)
+            //                                        .font(.headline)
+            //                                        .foregroundColor(.white)
+            //
+            //                                    Text(mission.formattedLaunchDate)
+            //                                        .font(.caption)
+            //                                        .foregroundColor(.white.opacity(0.5))
+            //                                }
+            //                                .padding(.vertical)
+            //                                .frame(maxWidth: .infinity)
+            //                                .background(.lightBackground)
+            //                            }
+            //                            .clipShape(RoundedRectangle(cornerRadius: 10))
+            //                            .overlay(
+            //                                RoundedRectangle(cornerRadius: 10)
+            //                                    .stroke(.lightBackground)
+            //                            )
+            //                        }
+            //                    }
+            //                }
+            //                .padding([.horizontal, .bottom])
+            //            }
+            //            .navigationTitle("Moonshot")
+            //            .background(.darkBackground)
+            //            .preferredColorScheme(.dark)
+            //
+            //        }
     }
 }
 
---
+++ Moonshot/ListLayout.swift
@@ -0,0 +1,59 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by JooMin Choi on 07/10/2022.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                        
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundColor(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                }
+            }
+        }
+        .listStyle(.plain)
+//        .background(Color.darkBackground)
+//        .listRowBackground(Color.lightBackground)
+        .padding([.horizontal, .bottom])
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ListLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        ListLayout()
+    }
+}
